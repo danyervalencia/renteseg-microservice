84efa49af8465466f5bb659ba1db7c2b7568026e
Danyer Valencia <danyervalencia@gmail.com>
2023-06-13 06:59:18 -0500

    Primer commit de proyecto

---
+++ src/main/java/com/osiptel/renteseg/microservicie/MicroServicieApplication.java
@@ -0,0 +1,13 @@
+package com.osiptel.renteseg.microservicie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osiptel/renteseg/microservicie/MicroServicieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osiptel.renteseg.microservicie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
